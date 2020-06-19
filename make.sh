2d8be6894c40273da049867b3839203a1a84ae55
tranphongvnr <67084965+tranphongvnr@users.noreply.github.com>
2020-06-19 12:35:02 +0700

    Add files via upload

--- make.sh
+++ make.sh
@@ -1,51 +1,241 @@
 #!/bin/bash
 
-systempath=$1
-LOCALDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-scriptsdir=$LOCALDIR/../../../scripts
-toolsdir=$LOCALDIR/../../../tools
-TMPDIR=$LOCALDIR/../../../tmp/brightnessfix
-mkdir -p "$TMPDIR"
+# Project OEM-GSI Porter by Erfan Abdi <erfangplus@gmail.com>
 
-BAKSMALIJAR="$toolsdir"/smali/baksmali.jar
-SMALIJAR="$toolsdir"/smali/smali.jar
-APKTOOL="$toolsdir"/apktool/apktool.jar
+usage()
+{
+echo "Usage: $0 <Path to GSI system> <Firmware type> <Output type> [Output Dir]"
+    echo -e "\tPath to GSI system: Mount GSI and set mount point"
+    echo -e "\tFirmware type: Firmware mode"
+    echo -e "\tOutput type: AB or Aonly"
+    echo -e "\tOutput Dir: set output dir"
+}
 
-$scriptsdir/oat2dex.sh "$systempath/framework" "$systempath/framework/services.jar" 2>/dev/null >> "$TMPDIR"/oat2dex.log
-mkdir -p "$TMPDIR/original_dex"
-7z e "$systempath/framework/services.jar" classes* -o"$TMPDIR/original_dex" 2>/dev/null >> "$TMPDIR"/zip.log
-CLASSES=$(ls "$TMPDIR/original_dex/classes"*)
-for CLASS in $CLASSES; do
-    java -jar "$BAKSMALIJAR" disassemble "$CLASS" -o "$TMPDIR/dexout"
-    LightImpl=$(grep  ".method private setLightLocked(IIIIIZ)V" "$TMPDIR"/dexout/com/android/server/lights/ -ri | cut -d ":" -f 1)
-    if [[ ! $LightImpl == "" ]]; then
-        cp $LightImpl "$TMPDIR"/LightImpl.smali
-        rm $LightImpl
+if [ "$3" == "" ]; then
+    echo "ERROR: Enter all needed parameters"
+    usage
+    exit 1
+fi
+
+LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+sourcepath=$1
+romtype=$2
+outputtype=$3
+
+if [[ $romtype == *":"* ]]; then
+    romtypename=`echo "$romtype" | cut -d ":" -f 2`
+    romtype=`echo "$romtype" | cut -d ":" -f 1`
+else
+    romtypename=$romtype
+fi
+
+flag=false
+roms=("$LOCALDIR"/roms/*/*)
+for dir in "${roms[@]}"
+do
+    rom=`echo "$dir" | rev | cut -d "/" -f 1 | rev`
+    if [ "$rom" == "$romtype" ]; then
         flag=true
-        while IFS= read -r line;
-        do
-            $flag && echo "$line" >> $LightImpl
-            if [[ "$line" == ".method public setBrightness(II)V" ]]; then
-                flag=false
-                cat "$LOCALDIR"/LightsService_LightImpl.patch >> $LightImpl
-            fi
-            if ! $flag && [[ "$line" == ".end method" ]]; then
-                flag=true
-                echo "$line" >> $LightImpl
-            fi
-        done  < "$TMPDIR"/LightImpl.smali
-        NEWCLASS=$(echo "$CLASS" | rev | cut -d "/" -f 1 | rev)
-        java -jar "$SMALIJAR" assemble "$TMPDIR/dexout" -o "$TMPDIR/$NEWCLASS"
-        zip -gjq "$systempath/framework/services.jar" "$TMPDIR/$NEWCLASS"
-        java -jar "$APKTOOL" d $systempath/framework/framework-res.apk -o $TMPDIR/framework-res 2>/dev/null >> "$TMPDIR"/apktoolunpack.log
-        ROMMAXBRIGHTNESS=$(grep '<integer name="config_screenBrightnessSettingMaximum">' $TMPDIR/framework-res/res/values/integers.xml | sed 's/<integer name="config_screenBrightnessSettingMaximum">//g' | sed 's/<\/integer>//' | sed 's/^[[:space:]]*//g')
-        echo "persist.display.rom_max_brightness=$ROMMAXBRIGHTNESS" >> $systempath/build.prop
-        rm -rf "$TMPDIR"
-        exit
     fi
-    rm -rf "$TMPDIR/dexout"
 done
+if [ "$flag" == "false" ]; then
+    echo "$romtype is not supported rom, supported roms:"
+    for dir in "${roms[@]}"
+    do
+        ver=`echo "$dir" | rev | cut -d "/" -f 2 | rev`
+        rom=`echo "$dir" | rev | cut -d "/" -f 1 | rev`
+        echo "$rom for Android $ver"
+    done
+    exit 1
+fi
+flag=false
+case "$outputtype" in
+    *"AB"*) flag=true ;;
+    *"Aonly"*) flag=true ;;
+esac
+if [ "$flag" == "false" ]; then
+    echo "$outputtype is not supported type, supported types:"
+    echo "AB"
+    echo "Aonly"
+    exit 1
+fi
 
-# This should not happen
-echo "ERR: Patching Light not supported for this rom"
-rm -rf "$TMPDIR"
+# Detect Source type, AB or not
+sourcetype="Aonly"
+if [[ -e "$sourcepath/system" ]]; then
+    sourcetype="AB"
+fi
+
+tempdirname="tmp"
+tempdir="$LOCALDIR/$tempdirname"
+systemdir="$tempdir/system"
+toolsdir="$LOCALDIR/tools"
+romsdir="$LOCALDIR/roms"
+prebuiltdir="$LOCALDIR/prebuilt"
+scriptsdir="$LOCALDIR/scripts"
+
+echo "Create Temp dir"
+rm -rf $tempdir
+mkdir -p "$systemdir"
+
+if [ "$sourcetype" == "Aonly" ]; then
+    echo "Warning: Aonly source detected, using P AOSP rootdir"
+    cd "$systemdir"
+    tar xf "$prebuiltdir/ABrootDir.tar"
+    cd "$LOCALDIR"
+    echo "Making copy of source rom to temp"
+    ( cd "$sourcepath" ; sudo tar cf - . ) | ( cd "$systemdir/system" ; sudo tar xf - )
+    cd "$LOCALDIR"
+    sed -i "/ro.build.system_root_image/d" "$systemdir/system/build.prop"
+    sed -i "/ro.build.ab_update/d" "$systemdir/system/build.prop"
+    echo "ro.build.system_root_image=false" >> "$systemdir/system/build.prop"
+else
+    echo "Making copy of source rom to temp"
+    ( cd "$sourcepath" ; sudo tar cf - . ) | ( cd "$systemdir" ; sudo tar xf - )
+    cd "$LOCALDIR"
+    sed -i "/ro.build.system_root_image/d" "$systemdir/system/build.prop"
+    sed -i "/ro.build.ab_update/d" "$systemdir/system/build.prop"
+    echo "ro.build.system_root_image=true" >> "$systemdir/system/build.prop"
+fi
+
+# Detect is the src treble ro.treble.enabled=true
+istreble=`cat $systemdir/system/build.prop | grep ro.treble.enabled | cut -d "=" -f 2`
+if [[ ! "$istreble" == "true" ]]; then
+    echo "The source is not treble supported"
+    exit 1
+fi
+
+# Detect Source API level
+if grep -q ro.build.version.release_or_codename $systemdir/system/build.prop; then
+    sourcever=`grep ro.build.version.release_or_codename $systemdir/system/build.prop | cut -d "=" -f 2`
+else
+    sourcever=`grep ro.build.version.release $systemdir/system/build.prop | cut -d "=" -f 2`
+fi
+if [ $(echo $sourcever | cut -d "." -f 2) == 0 ]; then
+    sourcever=$(echo $sourcever | cut -d "." -f 1)
+fi
+flag=false
+case "$sourcever" in
+    *"9"*) flag=true ;;
+    *"10"*) flag=true ;;
+    *"11"*) flag=true ;;
+esac
+if [ "$flag" == "false" ]; then
+    echo "$sourcever is not supported"
+    exit 1
+fi
+
+# Detect rom folder again
+if [[ ! -d "$romsdir/$sourcever/$romtype" ]]; then
+    echo "$romtype is not supported rom for android $sourcever"
+    exit 1
+fi
+
+# Detect arch
+if [[ ! -f "$systemdir/system/lib64/libandroid.so" ]]; then
+    echo "32bit source detected, weird flex but ok!"
+    # do something here?
+fi
+
+# Debloat
+$romsdir/$sourcever/$romtype/debloat.sh "$systemdir/system" 2>/dev/null
+$romsdir/$sourcever/$romtype/$romtypename/debloat.sh "$systemdir/system" 2>/dev/null
+
+# Resign to AOSP keys
+if [[ ! -e $romsdir/$sourcever/$romtype/$romtypename/DONTRESIGN ]]; then
+    if [[ ! -e $romsdir/$sourcever/$romtype/DONTRESIGN ]]; then
+        echo "Resigning to AOSP keys"
+        ispython3=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
+        if [ $ispython3 -eq 0 ]; then
+            python3=python3
+        else
+            python3=python
+        fi
+        $python3 $toolsdir/ROM_resigner/resign.py "$systemdir/system" $toolsdir/ROM_resigner/AOSP_security > $tempdir/resign.log
+        $prebuiltdir/resigned/make.sh "$systemdir/system" 2>/dev/null
+    fi
+fi
+
+# Start patching
+echo "Patching started..."
+$scriptsdir/fixsymlinks.sh "$systemdir/system" 2>/dev/null
+$scriptsdir/nukeABstuffs.sh "$systemdir/system" 2>/dev/null
+$prebuiltdir/vendor_vndk/make$sourcever.sh "$systemdir/system" 2>/dev/null
+$prebuiltdir/$sourcever/make.sh "$systemdir/system" "$romsdir/$sourcever/$romtype" 2>/dev/null
+$prebuiltdir/$sourcever/makeroot.sh "$systemdir" "$romsdir/$sourcever/$romtype" 2>/dev/null
+$prebuiltdir/common/make.sh "$systemdir/system" "$romsdir/$sourcever/$romtype" 2>/dev/null
+$romsdir/$sourcever/$romtype/make.sh "$systemdir/system" 2>/dev/null
+$romsdir/$sourcever/$romtype/makeroot.sh "$systemdir" 2>/dev/null
+if [ ! "$romtype" == "$romtypename" ]; then
+    $romsdir/$sourcever/$romtype/$romtypename/make.sh "$systemdir/system" 2>/dev/null
+    $romsdir/$sourcever/$romtype/$romtypename/makeroot.sh "$systemdir" 2>/dev/null
+fi
+if [ "$outputtype" == "Aonly" ] && [ ! "$romtype" == "$romtypename" ]; then
+    $romsdir/$sourcever/$romtype/$romtypename/makeA.sh "$systemdir/system" 2>/dev/null
+fi
+if [ "$outputtype" == "Aonly" ]; then
+    $prebuiltdir/$sourcever/makeA.sh "$systemdir/system" 2>/dev/null
+    $romsdir/$sourcever/$romtype/makeA.sh "$systemdir/system" 2>/dev/null
+fi
+
+# Fixing environ
+if [ "$outputtype" == "Aonly" ]; then
+    if [[ ! $(ls "$systemdir/system/etc/init/" | grep *environ*) ]]; then
+        echo "Generating environ.rc"
+        echo "# AUTOGENERATED FILE BY ERFANGSI TOOLS" > "$systemdir/system/etc/init/init.treble-environ.rc"
+        echo "on init" >> "$systemdir/system/etc/init/init.treble-environ.rc"
+        cat "$systemdir/init.environ.rc" | grep BOOTCLASSPATH >> "$systemdir/system/etc/init/init.treble-environ.rc"
+        cat "$systemdir/init.environ.rc" | grep SYSTEMSERVERCLASSPATH >> "$systemdir/system/etc/init/init.treble-environ.rc"
+    fi
+fi
+
+date=`date +%Y%m%d`
+outputname="$romtypename-$outputtype-$sourcever-$date-TranPhong"
+outputimagename="$outputname".img
+outputtextname="$outputname".txt
+if [ "$4" == "" ]; then
+    echo "Create out dir"
+    outdirname="out"
+    outdir="$LOCALDIR/$outdirname"
+    mkdir -p "$outdir"
+else
+    outdir="$4"
+fi
+output="$outdir/$outputimagename"
+outputinfo="$outdir/$outputtextname"
+
+$scriptsdir/getinfo.sh "$systemdir/system" > "$outputinfo"
+
+if [[ $(grep "ro.build.display.id" $systemdir/system/build.prop) ]]; then
+    displayid="ro.build.display.id"
+elif [[ $(grep "ro.system.build.id" $systemdir/system/build.prop) ]]; then
+    displayid="ro.system.build.id"
+elif [[ $(grep "ro.build.id" $systemdir/system/build.prop) ]]; then
+    displayid="ro.build.id"
+fi
+displayid2=$(echo "$displayid" | sed 's/\./\\./g')
+bdisplay=$(grep "$displayid" $systemdir/system/build.prop | sed 's/\./\\./g; s:/:\\/:g; s/\,/\\,/g; s/\ /\\ /g')
+sed -i "s/$bdisplay/$displayid2=Built\.with\.ErfanGSI\.Tools\.by\.TranPhong/" $systemdir/system/build.prop
+
+# Getting system size and add approximately 5% on it just for free space
+systemsize=`du -sk $systemdir | awk '{$1*=1024;$1=int($1*1.05);printf $1}'`
+bytesToHuman() {
+    b=${1:-0}; d=''; s=0; S=(Bytes {K,M,G,T,P,E,Z,Y}iB)
+    while ((b > 1024)); do
+        d="$(printf ".%02d" $((b % 1024 * 100 / 1024)))"
+        b=$((b / 1024))
+        let s++
+    done
+    echo "$b$d ${S[$s]}"
+}
+echo "Raw Image Size: $(bytesToHuman $systemsize)" >> "$outputinfo"
+
+echo "Creating Image: $outputimagename"
+# Use ext4fs to make image in P or older!
+if [ "$sourcever" == "9" ]; then
+    useold="--old"
+fi
+$scriptsdir/mkimage.sh $systemdir $outputtype $systemsize $output $useold > $tempdir/mkimage.log
+
+echo "Remove Temp dir"
+rm -rf "$tempdir"
